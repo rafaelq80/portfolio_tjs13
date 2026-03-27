2c58f611bcb49596382a5365c0149b17ccd024f6
Rafael Queiroz <rafaelproinfo@gmail.com>
2026-03-27 18:10:05 -0300

    👌 IMPROVE: Finalização do Script JS - Validação e Envio do formulário

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,9 @@ const swiperWrapper = document.querySelector('.swiper-wrapper')
 // Formulário
 const formulario = document.querySelector('#formulario')
 
+// Expressão Regular de validação do e-mail
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
 // Função de preenchimento da seção about
 async function getAboutGitHub() {
 	try {
@@ -259,6 +262,61 @@ function iniciarSwiper() {
         slidesOffsetAfter: 0, 
     });
 }
+
+formulario.addEventListener('submit', function(event) {
+  
+  event.preventDefault();
+
+  document.querySelectorAll('form span')
+    .forEach(span => span.innerHTML = '');
+
+  let isValid = true;
+
+  const nome = document.querySelector('#nome');
+  const erroNome = document.querySelector('#erro-nome');
+
+  if (nome.value.trim().length < 3){
+    erroNome.innerHTML = 'O nome deve ter no mínimo 3 caracteres'
+    if (isValid) nome.focus();
+    isValid = false;
+  }
+
+  const email = document.querySelector('#email');
+  const erroEmail = document.querySelector('#erro-email');
+
+  if (!email.value.trim().match(emailRegex)){
+    erroEmail.innerHTML = 'Digite um endereço de e-mail válido'
+    if (isValid) email.focus();
+    isValid = false;
+  }
+
+  const assunto = document.querySelector('#assunto');
+  const erroAssunto = document.querySelector('#erro-assunto');
+
+  if (assunto.value.trim().length < 5){
+    erroAssunto.innerHTML = 'O assunto deve ter no mínimo 5 caracteres'
+    if (isValid) assunto.focus();
+    isValid = false;
+  }
+
+  const mensagem = document.querySelector('#mensagem');
+  const erroMensagem = document.querySelector('#erro-mensagem');
+
+  if (mensagem.value.trim().length === 0){
+    erroMensagem.innerHTML = 'A mensagem não pode ser vazia'
+    if (isValid) mensagem.focus();
+    isValid = false;
+  }
+
+  if (isValid){
+    const submitButton = formulario.querySelector('button[type="submit"]');
+    submitButton.disabled = true;
+    submitButton.textContent = 'Enviando...';
+
+    formulario.submit();
+  }
+
+})
 
 // executar a função getAboutGitHub
 getAboutGitHub();
